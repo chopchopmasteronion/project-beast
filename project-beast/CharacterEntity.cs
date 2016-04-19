b07c7451f2d831ca97897e38f1ac09afdba0da8c
Flex <flexedoyster@hotmail.com>
2016-04-18 22:32:02 -0400

    accurate animations
    added final set of walking animations, need to move this out to json

--- project-beast/CharacterEntity.cs
+++ project-beast/CharacterEntity.cs
@@ -11,6 +11,15 @@ namespace Player
     {
         static Texture2D characterSheetTexture;
         Animation walkDown;
+        Animation walkUp;
+        Animation walkLeft;
+        Animation walkRight;
+
+        Animation standDown;
+        Animation standUp;
+        Animation standLeft;
+        Animation standRight;
+
         Animation currentAnimation;
         float speed;
 
@@ -35,12 +44,45 @@ namespace Player
                     characterSheetTexture = Texture2D.FromStream(graphicsDevice, stream);
                 }
             }
-            speed = 20;
+
+            speed = 40;
+
             walkDown = new Animation();
             walkDown.AddFrame(new Rectangle(0, 0, 16, 16), TimeSpan.FromSeconds(.25));
             walkDown.AddFrame(new Rectangle(16, 0, 16, 16), TimeSpan.FromSeconds(.25));
             walkDown.AddFrame(new Rectangle(0, 0, 16, 16), TimeSpan.FromSeconds(.25));
             walkDown.AddFrame(new Rectangle(32, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            walkUp = new Animation();
+            walkUp.AddFrame(new Rectangle(144, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkUp.AddFrame(new Rectangle(160, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkUp.AddFrame(new Rectangle(144, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkUp.AddFrame(new Rectangle(176, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            walkLeft = new Animation();
+            walkLeft.AddFrame(new Rectangle(48, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkLeft.AddFrame(new Rectangle(64, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkLeft.AddFrame(new Rectangle(48, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkLeft.AddFrame(new Rectangle(80, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            walkRight = new Animation();
+            walkRight.AddFrame(new Rectangle(96, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkRight.AddFrame(new Rectangle(112, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkRight.AddFrame(new Rectangle(96, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkRight.AddFrame(new Rectangle(128, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            // Standing animations only have a single frame of animation:
+            standDown = new Animation();
+            standDown.AddFrame(new Rectangle(0, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            standUp = new Animation();
+            standUp.AddFrame(new Rectangle(144, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            standLeft = new Animation();
+            standLeft.AddFrame(new Rectangle(48, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            standRight = new Animation();
+            standRight.AddFrame(new Rectangle(96, 0, 16, 16), TimeSpan.FromSeconds(.25));
         }
 
         Vector2 GetDesiredVelocityFromInput()
@@ -97,9 +139,61 @@ namespace Player
             this.X += velocity.X * (float)gameTime.ElapsedGameTime.TotalSeconds;
             this.Y += velocity.Y * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            // temporary - we'll replace this with logic based off of which way the
-            // character is moving when we add movement logic
-            currentAnimation = walkDown;
+
+            if (velocity != Vector2.Zero)
+            {
+                bool movingHorizontally = Math.Abs(velocity.X) > Math.Abs(velocity.Y);
+                if (movingHorizontally)
+                {
+                    if (velocity.X > 0)
+                    {
+                        currentAnimation = walkRight;
+                    }
+                    else
+                    {
+                        currentAnimation = walkLeft;
+                    }
+                }
+                else
+                {
+                    if (velocity.Y > 0)
+                    {
+                        currentAnimation = walkDown;
+                    }
+                    else
+                    {
+                        currentAnimation = walkUp;
+                    }
+                }
+            }
+            else
+            {
+                // If the character was walking, we can set the standing animation
+                // according to the walking animation that is playing:
+                if (currentAnimation == walkRight)
+                {
+                    currentAnimation = standRight;
+                }
+                else if (currentAnimation == walkLeft)
+                {
+                    currentAnimation = standLeft;
+                }
+                else if (currentAnimation == walkUp)
+                {
+                    currentAnimation = standUp;
+                }
+                else if (currentAnimation == walkDown)
+                {
+                    currentAnimation = standDown;
+                }
+                else if (currentAnimation == null)
+                {
+                    currentAnimation = standDown;
+                }
+
+                // if none of the above code hit then the character
+                // is already standing, so no need to change the animation.
+            }
 
             currentAnimation.Update(gameTime);
         }
